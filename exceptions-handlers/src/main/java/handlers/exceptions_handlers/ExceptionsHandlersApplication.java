607afebfc55c000144556a819447d2f39552262b
Jamyle Elen <jamyleelen09@gmail.com>
2025-02-20 13:43:57 -0300

    entendendo Exceptions Handles para a tratativa de erros

---
+++ exceptions-handlers/src/main/java/handlers/exceptions_handlers/ExceptionsHandlersApplication.java
@@ -0,0 +1,13 @@
+package handlers.exceptions_handlers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExceptionsHandlersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExceptionsHandlersApplication.class, args);
+	}
+
+}
